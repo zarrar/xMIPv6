bb1c46806cff77d972053251d93a625760ea93b0
Zoltan Bojthe <zbojthe@urdeso.hu>
2010-12-10 12:16:11 +0100

    IPv6Header: remove the unneccessary @customize

--- src/networklayer/ipv6/IPv6ExtensionHeaders.h
+++ src/networklayer/ipv6/IPv6ExtensionHeaders.h
@@ -20,41 +20,7 @@
 #ifndef _IPv6EXTENSIONHEADERS_H_
 #define _IPv6EXTENSIONHEADERS_H_
 
-#include <list>
-#include "INETDefs.h"
 #include "IPv6ExtensionHeaders_m.h"
-
-
-/**
- * Represents an IPv6 Destination Options Header. More info in the IPv6ExtensionHeaders.msg
- * file (and the documentation generated from it).
- */
-class INET_API IPv6DestinationOptionsHeader : public IPv6DestinationOptionsHeader_Base
-{
-  public:
-	IPv6DestinationOptionsHeader() : IPv6DestinationOptionsHeader_Base() {};
-	IPv6DestinationOptionsHeader(const IPv6DestinationOptionsHeader& other) : IPv6DestinationOptionsHeader_Base() {operator=(other);}
-	//IPv6DestinationOptionsHeader& operator=(const IPv6DestinationOptionsHeader& other);
-	IPv6DestinationOptionsHeader& operator=(const IPv6DestinationOptionsHeader& other) {IPv6DestinationOptionsHeader_Base::operator=(other); return *this;}
-
-    virtual IPv6DestinationOptionsHeader *dup() const {return new IPv6DestinationOptionsHeader(*this);}
-};
-
-
-/**
- * Represents an IPv6 Routing Header. More info in the IPv6ExtensionHeaders.msg file
- * (and the documentation generated from it).
- */
-class INET_API IPv6RoutingHeader : public IPv6RoutingHeader_Base
-{
-  public:
-	  IPv6RoutingHeader() : IPv6RoutingHeader_Base() {};
-	  IPv6RoutingHeader(const IPv6RoutingHeader& other) : IPv6RoutingHeader_Base() {operator=(other);}
-	  //IPv6RoutingHeader& operator=(const IPv6RoutingHeader& other);
-	  IPv6RoutingHeader& operator=(const IPv6RoutingHeader& other) {IPv6RoutingHeader_Base::operator=(other); return *this;}
-
-	  virtual IPv6RoutingHeader *dup() const {return new IPv6RoutingHeader(*this);}
-};
 
 #endif
 
